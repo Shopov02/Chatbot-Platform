62dbcee805dbdec5b5ea4b4b83e51c2a4dbd5043
Todor Shopov <todorshopov021@gmail.com>
2024-07-23 11:04:45 +0300

    up

---
+++ ChatbotPlatform/ChatbotPlatform.Api.Tests/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace ChatbotPlatform.Api.Tests
+{
+    public static class Constants
+    {
+        public const string BaseAddress = "";
+        public const string ApiBaseAddress = BaseAddress + "/api/";
+
+        public static class IndentityApi
+        {
+            public const string Register = "/users/register";
+            public const string Login = "/users/login";
+            public const string ChangePassword = "/users/change-password";
+            public const string ExistingEmail = "/users/emails/{0}";
+
+            public const string Email = "";
+            public const string Password = "";
+        }
+    }
+}
